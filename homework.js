c4f5a7a36798d60ff8e6e7532ff4ac6b36bf8ef1
romeodsmith <33501239+romeodsmith@users.noreply.github.com>
2020-11-15 18:49:18 -0500

    Add files via upload

---
+++ homework.js
@@ -0,0 +1,28 @@
+var pd=prompt("Enter Product: ");
+var pd= ("Laptop:");
+console.log(pd + " Dell Insprion Laptop 13.5 Inches, 1tb:");
+
+var qty=prompt("Enter Quantity: ");
+var qty=("Quantity: ")
+console.log(qty+1)
+
+var pr=prompt("Enter Price: ");
+var pr=(1000);
+console.log("Price: $" +pr);
+
+
+
+var st=prompt("Sales Tax: ");
+var st=(pr*.10);
+var tx= ("Tax:")
+console.log(tx + st);    
+
+
+
+
+var fp=("Final Price: ");
+
+console.log(pr+st);
+
+
+
